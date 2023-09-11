80647ffd3dbe10a0546e3a3db542773c32b25c9c
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-09-11 20:46:47 -0300

    dados dos pokemos

--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
+function PegarPokemons ()
+{
+    for (const pokemon of data.pokemon) {
+        console.log (pokemon.name);
+        console.log (pokemon.num);
+        console.log (pokemon.type)
+    }
+}
