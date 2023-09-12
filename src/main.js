27dda97de9eca5a0c29fae6d0ebed230cc829d6f
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-09-11 22:16:28 -0300

    Todos os pokeoms

--- src/main.js
+++ src/main.js
@@ -2,11 +2,32 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-function PegarPokemons ()
+function PegarPokemons()
 {
-    for (const pokemon of data.pokemon) {
+    for (const pokemon of data.pokemon){
+        console.log (pokemon.img);
         console.log (pokemon.name);
         console.log (pokemon.num);
-        console.log (pokemon.type)
+        console.log (pokemon.type);
+
+        const DivDoPokemon = document.createElement ('div');
+        DivDoPokemon.innerHTML = pokemon.name;
+
+        const Imagens = document.createElement ('img');
+        Imagens.src = pokemon.img;
+        DivDoPokemon.appendChild(Imagens);
+
+        const Numeros = document.createElement ('num');
+        Numeros.innerHTML = pokemon.num;
+        DivDoPokemon.appendChild(Numeros);
+
+        const Tipo = document.createElement ('type');
+        Tipo.innerHTML = pokemon.type;
+        DivDoPokemon.appendChild(Tipo);
+      
+
+        document.getElementById ('root').appendChild(DivDoPokemon);
     }
 }
+
+ PegarPokemons();
