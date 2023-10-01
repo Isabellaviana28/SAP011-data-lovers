da7c880e57bbd10aa951f18beee23ca71657c700
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-10-01 13:23:05 -0300

    organização dos cards

--- src/main.js
+++ src/main.js
@@ -2,28 +2,35 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-function PegarPokemons()
+
+export function somar(num1, num2){
+    return parseFloat(num1) + parseFloat(num2);
+}
+
+export function PegarPokemons()
 {
     for (const pokemon of data.pokemon){
-        console.log (pokemon.img);
-        console.log (pokemon.name);
-        console.log (pokemon.num);
-        console.log (pokemon.type);
 
         const DivDoPokemon = document.createElement ('div');
-        DivDoPokemon.innerHTML = pokemon.name;
+        
+        const Nome = document.createElement ('p');
+        Nome.className = "nome-pokemon";
+        Nome.innerHTML = pokemon.name;
+        DivDoPokemon.appendChild(Nome);
 
         const Imagens = document.createElement ('img');
         Imagens.src = pokemon.img;
         DivDoPokemon.appendChild(Imagens);
 
-        const Numeros = document.createElement ('num');
+        const Numeros = document.createElement ('p');
         Numeros.innerHTML = pokemon.num;
         DivDoPokemon.appendChild(Numeros);
 
-        const Tipo = document.createElement ('type');
+        const Tipo = document.createElement ('p');
         Tipo.innerHTML = pokemon.type;
         DivDoPokemon.appendChild(Tipo);
+        DivDoPokemon.className = "card-pokemon";
+
       
 
         document.getElementById ('root').appendChild(DivDoPokemon);
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,9 +1,16 @@
 import { example, anotherExample } from '../src/data.js';
+import { PegarPokemons } from '../src/main.js';
 
+describe('exibir pokemons', () => {
+  it('só pode exibir pokemons da regiao de kanto', () => {
+    // 1. Em que estado a aplicacao se encontra?
+    // Quais prerequisitos eu preciso cumprir pra rodar meu teste?
+    
+    // 2. Executar a ação que vc quer testar
+    PegarPokemons();
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+    // 3. Validar se a aplicação exibe o resultado correto
+    expect(document.getElementById('root').childElementCount).toBe(250);
   });
 
   it('returns `example`', () => {
