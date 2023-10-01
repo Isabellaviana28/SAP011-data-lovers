a110090433a33688bf0520606b7361dc1b0d7f0f
Isabellaviana28 <139775674+Isabellaviana28@users.noreply.github.com>
2023-10-01 14:26:01 -0300

    filtro dois

--- src/main.js
+++ src/main.js
@@ -1,64 +1,65 @@
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
-export function somar(num1, num2){
-    return parseFloat(num1) + parseFloat(num2);
+export function somar(num1, num2) {
+  return parseFloat(num1) + parseFloat(num2);
 }
 
-export function PegarPokemons()
-{
-   const alfabeto = document.getElementById ("ordenacao").value ;
-   if (alfabeto == "A-Z"){
-    data.pokemon.sort(function(pokemon1, pokemon2){
-        // se retornar -1, então p1 vem primeiro
-        // se retornor +1, então p2 vem primeiro
-        if(pokemon1.name < pokemon2.name){
-            return -1;
-        } else {
-            return 1;
-        }
+export function PegarPokemons() {
+  const filtro = document.getElementById("filtro").value;
+  const alfabeto = document.getElementById("ordenacao").value;
+  if (alfabeto == "A-Z") {
+    data.pokemon.sort(function (pokemon1, pokemon2) {
+      // se retornar -1, então p1 vem primeiro
+      // se retornor +1, então p2 vem primeiro
+      if (pokemon1.name < pokemon2.name) {
+        return -1;
+      } else {
+        return 1;
+      }
     });
-   } else {
-        data.pokemon.sort(function(pokemon1, pokemon2){
-            if(pokemon1.name > pokemon2.name){
-                return -1;
-            } else {
-                return 1;
-            }
-        });
-   }
+  } else {
+    data.pokemon.sort(function (pokemon1, pokemon2) {
+      if (pokemon1.name > pokemon2.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  }
 
-   // vai limpar o root antes de adicionar os pokemons
-   document.getElementById('root').innerHTML = "";
-    for (const pokemon of data.pokemon){
+  // vai limpar o root antes de adicionar os pokemons
+  document.getElementById("root").innerHTML = "";
+  for (const pokemon of data.pokemon) {
+    if (pokemon.type.indexOf(filtro) != -1 || filtro == "todos") {
+      const DivDoPokemon = document.createElement("div");
 
-        const DivDoPokemon = document.createElement ('div');
-        
-        const Nome = document.createElement ('p');
-        Nome.className = "nome-pokemon";
-        Nome.innerHTML = pokemon.name;
-        DivDoPokemon.appendChild(Nome);
+      const Nome = document.createElement("p");
+      Nome.className = "nome-pokemon";
+      Nome.innerHTML = pokemon.name;
+      DivDoPokemon.appendChild(Nome);
 
-        const Imagens = document.createElement ('img');
-        Imagens.src = pokemon.img;
-        DivDoPokemon.appendChild(Imagens);
+      const Imagens = document.createElement("img");
+      Imagens.src = pokemon.img;
+      DivDoPokemon.appendChild(Imagens);
 
-        const Numeros = document.createElement ('p');
-        Numeros.innerHTML = pokemon.num;
-        DivDoPokemon.appendChild(Numeros);
+      const Numeros = document.createElement("p");
+      Numeros.innerHTML = pokemon.num;
+      DivDoPokemon.appendChild(Numeros);
 
-        const Tipo = document.createElement ('p');
-        Tipo.innerHTML = pokemon.type;
-        DivDoPokemon.appendChild(Tipo);
-        DivDoPokemon.className = "card-pokemon";
+      const Tipo = document.createElement("p");
+      Tipo.innerHTML = pokemon.type;
+      DivDoPokemon.appendChild(Tipo);
+      DivDoPokemon.className = "card-pokemon";
 
-      
-
-        document.getElementById('root').appendChild(DivDoPokemon);
+      document.getElementById("root").appendChild(DivDoPokemon);
     }
+  }
 }
 
- PegarPokemons();
+PegarPokemons();
 
- document.getElementById("ordenacao").onchange = PegarPokemons;
+document.getElementById("ordenacao").onchange = PegarPokemons;
+
+document.getElementById("filtro").onchange = PegarPokemons;
